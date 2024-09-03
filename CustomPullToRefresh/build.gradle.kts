f80c4ac56f90e234a3c948459613937162a7340a
PrashantKaramadi <prashant.karamadi31@gmail.com>
2024-09-03 17:52:02 +0530

    Updating JAVA version.

--- CustomPullToRefresh/build.gradle.kts
+++ CustomPullToRefresh/build.gradle.kts
@@ -25,19 +25,14 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 
-    publishing {
-        singleVariant("release") {
-            withSourcesJar()
-        }
-    }
 }
 
 
@@ -49,6 +44,7 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
+
 publishing {
     publications {
         register<MavenPublication>("release") {
